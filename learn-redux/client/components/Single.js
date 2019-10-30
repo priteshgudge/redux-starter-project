341b9d70f02922e445e3464796bf61f94130a5e1
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-30 23:10:24 +0530

    Single Post page - post info setup

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Photo from './Photo';
 const Single = React.createClass({
     render(){
-
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const post = this.props.posts[i];
         return (
             <div className="single-photo">
-                Photo
+                <Photo post={post} i={i} {...this.props} />
             </div>
         )
     }
