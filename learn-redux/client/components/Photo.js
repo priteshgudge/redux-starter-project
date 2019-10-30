d03cb4d0c961721e27eecd0dc646b49da46144fc
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-30 22:42:22 +0530

    Setting up photo - part 1

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Link} from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Photo = React.createClass({
     render(){
+        const {post,i,comments} = this.props;
         return(
            <figure className="grid-figure">
-               Photo!!!
+               <div className="grid-photo-wrap">
+                   <Link to={`/view/${post.code}`}>
+                       <img src={post.display_src} alt={post.caption}
+                       className="grid-photo"/>
+                   </Link>
+                   <CSSTransitionGroup
+                        transitionName="like"
+                        transitionEnterTimeout={500}
+                        transitionLeaveTimeout={500}>
+                       <span key={post.likes} className="likes-heart">
+                           {post.likes}
+                       </span>
+
+                   </CSSTransitionGroup>
+               </div>
+               <figcaption>
+                   <p> {post.caption} </p>
+               </figcaption>
+               <div className="control-buttons">
+                   <button className="likes">&hearts;{post.likes}</button>
+                   <Link to={`/view/${post.code}`}>
+                        <span className="comment-count">
+                            
+                        </span>
+                   </Link>
+               </div>
            </figure>
         )
     }
