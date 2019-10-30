83ccbd1a56f274fc7f84e1f5ce2ca7c0b1f53210
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-30 16:13:04 +0530

    Base components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Main from './components/Main'
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+//React router deps
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 //IMport css
 import csss from './styles/style.styl'
 
-render(<Main/>, document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}> </Route>
+        </Route>
+    </Router>
+);
+
+render(router, document.getElementById('root'));
