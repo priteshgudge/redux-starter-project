5aa98caaaf6a2fd4c4a493403907661ae67141ea
Pritesh Gudge <priteshgudge@gmail.com>
2019-11-18 12:07:53 +0530

    Reducer to add comment

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,8 +1,34 @@
 // A recucer takes in (action, copy of current state)
 
+function postComments(state=[], action){
+    console.log("post comments");
+    switch (action.type) {
+        case "ADD_COMMENT":
+            // Return state with new comment
+            return [...state,{
+                user: action.author,
+                text: action.comment}];
+        case "REMOVE_COMMENT":
+            console.log("");
+            return state;
+        default:
+            return state;
+
+    }
+}
+
 function comments(state=[],action){
     console.log("Comment will change");
     //console.log(state, action);
+    if (typeof (action.postId !== "undefined")){
+        return {
+            // Take the current state
+            ...state,
+            // OVerwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action),
+
+        }
+    }
     return state;
 }
 
